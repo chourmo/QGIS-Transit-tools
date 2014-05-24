4e0bddc5eba32f91e02fc6fd051b662b3dbc3ac3
chourmo <vincenttinet@icloud.com>
2014-05-24 23:01:57 +0200

    Shortest Transit
    Correct errors and make it faster

--- scripts/Shortest times - transit.py
+++ scripts/Shortest times - transit.py
@@ -15,9 +15,15 @@ from qgis.core import *
 from qgis.networkanalysis import * 
 from PyQt4.QtCore import *
 from operator import itemgetter
-import time
 from math import sqrt
+import time
 
+
+def indexFeat(index, i, pt):
+	f = QgsFeature()
+	f.setFeatureId(i)
+	f.setGeometry(QgsGeometry.fromPoint(pt))
+	index.insertFeature(f)
 
 
 def buffRect(point, b):
@@ -26,51 +32,52 @@ def buffRect(point, b):
 	return QgsRectangle(x - b, y - b, x + b, y + b)
 
 
-def accumulateArcs(graph, start, dests, tree, arcfeat):
+def accumulateArcs(graph, start, dests, tree, i):
 	''' Accumulates values on the shortest path
 	G: Graph
 	start : starting node of graph
  	dest : list of nodes to accumulate in graph value
  	tree : list of previous arcs
- 	arcfeat : atrributes dict, key arcid, value for accumulation, must support adding
- 	
- 	Return dict, key node, value dict of attributes or list of -1 if not of accessible'''
+ 	i : start number in arc attributes for accumulation
+ 	Return dict, key node in dests list, value list of attributes''' 	
  	
 	dest_nodes = set(dests)  			# accessible nodes to analyze
-	arc_set = set()                     # analyzed arcs, id in graph
 	
-	results_t = {k: v[:] for k,v in arcfeat.iteritems()}
+	# pre-fill results dict
+	res = {k:-1 for k in range(graph.vertexCount())}
 		
 	while len(dest_nodes) > 0:
 		
-		n_cursor = dest_nodes.pop()       # first node to analyse
-		a_cursor = tree[n_cursor]         # analysed arc
-		arc_iter = []                     # list of arcs analyzed on this iteration
-		
+		path = []				  	      # list of nodes on path to start
+		n_cursor = dest_nodes.pop()       # first node to traverse
+																							
 		while n_cursor != start:
-			
-			# add each value of arc to arcs in list
-			
-			for b in arc_iter:
-				results_t[b] = [i[0]+i[1] for i in zip(results_t[b],results_t[a_cursor])]
-			
-			# Node already traversed, stop loop
-			
-			if a_cursor in arc_set: n_cursor = start
-			else:
-				arc_set.add(a_cursor)
-				arc_iter.append(a_cursor)   				
-				dest_nodes.discard(n_cursor)				
-				n_cursor = graph.arc(a_cursor).outVertex()
-				a_cursor = tree[n_cursor]
-	
-	results_t[-1] = [-1] * len(results_t[results_t.keys()[0]])
-	return {x:results_t[tree[x]] for x in dests}
+			dest_nodes.discard(n_cursor)  # if n_cursor a destination, delete from list
+						
+			# add arc value to res
+			res[n_cursor] = graph.arc(tree[n_cursor]).properties()[i:]
 
+			for n in path:
+				res[n] = [x + y for x,y in zip(res[n], res[n_cursor])]
+	
+			# add node in path
+			path.append(n_cursor)
+
+			# next vertex on path
+			n_cursor = graph.arc(tree[n_cursor]).outVertex()
+				
+			# if already traversed arc, end loop
+			if n_cursor != start and res[n_cursor] != -1:
+								
+				for n in path:
+					res[n] = [x + y for x,y in zip(res[n], res[n_cursor])]
+				n_cursor = start
+		
+	return {k:res[k] for k in dests}
 
 
 progress.setText('Built graph')
-
+t = time.time()
 
 buff = Max_walking_distance / 2.0
 maxcost = Max_total_time
@@ -78,19 +85,18 @@ tmax = Max_waiting_time
 cmax = Max_transfers
 
 # internal parameters
-
 b_res = 25.0        # proximity buffer to simplify result to close points
 b_pr = 500.0        # proximity buffer for transit in range of a PR node
-pen_car = 2.0       # penalisation of car mode for shortest path
+pen_car = 2.0       # penality for car mode
 r_VP = 0.5          # maximum amount of P+R in total time
 m_VP = 20           # maximum amount of car time (minutes)
 sep = "/"			# separator for modes list
 
 
-
 networklayer = processing.getObject(Transit_network)
 networkprovider = networklayer.dataProvider()
 fieldnames = networkprovider.fieldNameMap()
+n = networklayer.featureCount()
 
 if networklayer.fieldNameIndex("from")==-1: progress.setText("No field from")
 if networklayer.fieldNameIndex("to")==-1: progress.setText("No field to")
@@ -98,115 +104,86 @@ if networklayer.fieldNameIndex("dir")==-1: progress.setText("No field dir")
 if networklayer.fieldNameIndex("freq")==-1: progress.setText("No field freq")
 if networklayer.fieldNameIndex("mode")==-1: progress.setText("No field mode")
 
-idfrom = networklayer.fieldNameIndex("from")
-idto = networklayer.fieldNameIndex("to")
-iddir = networklayer.fieldNameIndex("dir")
-idfreq = networklayer.fieldNameIndex("freq")
-idmode = networklayer.fieldNameIndex("mode")
 
 G = QgsGraph()
-node_index = set()           # set of nodes to index
 Nodes = {}                   # key Network.id(), value node graoh id
-node_freq = {}               # key Network.id(), value frequency of node
-Arc_feat = {}                # dict of feat attributes, key id of arcs in graph
+node_freq = {}               # key node graph id, value frequency of node
+Arc_ix = []					 # list of arcs to add to graph
 
-feat_index = QgsFeature()
-index = QgsSpatialIndex()
+
 l = 0
-n = networklayer.featureCount()
-
-# order of values in Arc_feat
-n_corr = 0
-n_t_corr = 1
-n_mode = 2
-n_Costvp = 3
-n_Cost = 4
-
-
-
-
 for feat in processing.features(networklayer):
 	progress.setPercentage(int(100*l/n))
 	l+=1
 	
-	direction = feat[iddir]
-	mode = feat[idmode]
+	direction = feat["dir"]
+	mode = feat["mode"]
 	cost = feat[Cost]
 	
 	# arc in graph only if it has a cost, a direction and if not PR arcs or not of VP type
 	
-	if cost >= 0 and direction != 0 and (Park_ride or mode !='road'):    
-		n_begin = feat[idfrom]
-		n_end = feat[idto]
+	if direction != 0 and (Park_ride or mode !='road'):    
+		n_begin = feat["from"]
+		n_end = feat["to"]
 		geom = feat.geometry() 
 
-		
-		# building nodes, only if not already built
-		
-		Nodes.setdefault(n_begin, G.addVertex(geom.vertexAt(0)))
+		# building nodes
+		Nodes[n_begin] = {'p':geom.vertexAt(0), 'mode':mode, 'fr':30.0 / feat["freq"]}
 		if geom.isMultipart(): geom = geom.asGeometryCollection()[-1]
-		Nodes.setdefault(n_end, G.addVertex(geom.vertexAt(len(geom.asPolyline())-1)))
-
-		graphbegin = Nodes[n_begin]
-		graphend = Nodes[n_end]
-
-		if mode not in ['road', 'parking', 'transfer']:
-			node_index.add(graphbegin)
-			node_index.add(graphend)
-			node_freq[graphbegin] = node_freq[graphend] = 30.0 / feat[idfreq]
-		
-
-		# add arcs in graph depending on direction
-
-		newarcs = []
+		Nodes[n_end] = {'p':geom.vertexAt(len(geom.asPolyline())-1),
+					    'mode':mode,
+					    'fr':30.0 / feat["freq"]}
 				
-		# penalize road cost
-		
+		# penalize road cost		
 		if Park_ride and mode =='road': costs = [cost * pen_car]
 		else : costs = [cost]
-				
-		if direction == 1 or direction == 2:
-			newarcs.append(G.addArc(graphbegin, graphend, costs))
-			
-		if direction == -1 or direction == 2:
-			newarcs.append(G.addArc(graphend, graphbegin, costs))
 		
-		# values of Arc_feat :[count transfer, transfer cost, mode text]
-		# if park and ride: + [road cost, true cost]
+		if mode == 'transfer': costs.extend([1, cost, sep])
+		elif mode == 'parking': costs.extend([0, cost, sep])
+		else: costs.extend([0, 0, mode + sep])
+	
+		if Park_ride and mode == 'road': costs.extend([cost, cost])
+		elif Park_ride: costs.extend([0, cost])
 		
-		for a in newarcs:
-			if mode == 'transfer': Arc_feat[a] = [1, cost, sep]
-			elif mode == 'parking': Arc_feat[a] = [0, cost, sep]
-			else: Arc_feat[a] = [0, 0, mode + sep]
-			
-			if Park_ride:
-				if mode == 'road': Arc_feat[a].extend([cost, cost])
-				else: Arc_feat[a].extend([0, cost])
+		# cost with park and ride = [cost with car penality, number of transfers, transfer cost, mode text, road cost, full cost]
+		# cost transit only = [cost, number of transfers, transfer cost, mode text]
 
-progress.setText('Index nodes')
+		# add arcs in graph depending on direction								
+		if direction == 1 or direction == 2: Arc_ix.append([n_begin, n_end, costs])
+		if direction == -1 or direction == 2: Arc_ix.append([n_end, n_begin, costs])
 
+ix = QgsSpatialIndex()
 
-# Built index of nodes to connect starting points
+# Add nodes to graph
+for k in  Nodes:
 
-for n in node_index:
-	feat_index.setFeatureId(n)
-	feat_index.setGeometry(QgsGeometry.fromPoint(G.vertex(n).point()))
-	index.insertFeature(feat_index)
+	# store node parameters
+	p = Nodes[k]['p']
+	mode = Nodes[k]['mode']
+	freq = Nodes[k]['fr']
+	
+	# add node to graph
+	Nodes[k] = G.addVertex(p)
+	node_freq[Nodes[k]] = freq
+	
+	if mode != 'road': indexFeat(ix, Nodes[k], p)
+
+# Add arcs to graph
+for a in Arc_ix:
+	G.addArc(Nodes[a[0]], Nodes[a[1]], a[2])
 
 
 
 # Connect objects to nodes in graph inside buffer b
-
 progress.setText('Add start points')
 
-
-max_n = max(Nodes)
 startpts = []
+max_n = max(Nodes)
 
 startslayer = processing.getObject(Starts)
 n = startslayer.featureCount()
-l = 0
 
+l = 0
 for feat in processing.features(startslayer):
 	progress.setPercentage(int(100*l/n))
 	l+=1
@@ -214,39 +191,35 @@ for feat in processing.features(startslayer):
 	# find nodes in buffer
 	
 	c = feat.geometry().centroid().asPoint()
-	near = index.intersects(buffRect(c, buff))
+	near = ix.intersects(buffRect(c, buff))
 	
-	if len(near)!=0:
-		max_n += 1
+	if len(near)==0:
+		progress.setText("Start point {0} not connected".format(feat[Name]))
+	
+	else:
 		
 		# create new node
+		max_n += 1
 		Nodes[max_n] = G.addVertex(c)
 
 		# create startpts dict to store results
-		startpts.append({'vertex':Nodes[max_n], 'name':feat[Name]})
-		
+		startpts.append({'vertex': Nodes[max_n], 'name': feat[Name]})
 						
 		for i in near:
 		
-			dist = sqrt(c.sqrDist(G.vertex(i).point()))
+			# waiting time		
+			wait = min(tmax, node_freq[i] + sqrt(c.sqrDist(G.vertex(i).point())) * 0.015)
 			
-			# create arc
+			if Park_ride: cost = [wait, 0, wait, 'walk', 0, wait]
+			else: cost = [wait, 0, wait, '']
 			
-			wait_t = min(tmax, node_freq[i] + dist * 0.015)   # waiting time
-			
-			pos = G.addArc(Nodes[max_n], i, [wait_t])
-
-			if Park_ride: Arc_feat[pos] = [0, wait_t, "walk" + sep, 0, wait_t]
-			else: Arc_feat[pos] = [0, wait_t, "walk" + sep]
-							
-	else: progress.setText("Start point {0} not connected".format(feat[Name]))
-
+			G.addArc(Nodes[max_n], i, cost)
 
 
 
 # Shortest time per start point
 
-progress.setText('Shortest times')
+progress.setText('Shortest times...')
 
 
 # list of destination nodes in graph value
@@ -254,83 +227,76 @@ progress.setText('Shortest times')
 nodes_d = set(Nodes.values())
 max_n = len(Nodes)
 n = len(startpts)
+
+accnodes = set()
+
 l = 0
 
 
 for st in startpts:
 	progress.setPercentage(int(100*l/n))
 	l+=1
-
-	(tree, st['l']) = QgsGraphAnalyzer.dijkstra(G, st['vertex'], 0)
+	
+	stpt = st['vertex']
+	
+	(tree, st['cost']) = QgsGraphAnalyzer.dijkstra(G, stpt, 0)
 
     # dont process start node
-	tree[st['vertex']] = st['l'][st['vertex']] = -1
+	tree[stpt] = st['cost'][stpt] = -1
     
-	# process only accessible nodes with length less than maxcost
-	list_acc = [i for i in nodes_d if tree[i] != -1 and st['l'][i] <= maxcost]
+	# accessible nodes with length less than maxcost
+	valid = set([i for i in nodes_d if tree[i] != -1 and st['cost'][i] <= maxcost])
 	
-    # convert non accessible values of length to -1 instead of inf
-	for i in nodes_d - set(list_acc): st['l'][i] = -1
-    
-	st['res'] = accumulateArcs(G, st['vertex'], list_acc, tree, Arc_feat)
-
+	# secondary values
+	st['param'] = accumulateArcs(G, stpt, valid, tree, 1)
 		
 	# dont keep transit nodes with to many transfers
+	valid = set([x for x in valid if st['param'][x][0] <= cmax])
 
-	for i in [x for x in list_acc if st['res'][x][n_corr] > cmax]:
-		st['l'][i] = -1
-		list_acc.remove(i)
-
-
-	# eliminate transit nodes in small buffer with bigger value
+	# eliminate transit nodes in small buffer with similar value
+	removenode = set()
+	for i in [x for x in valid if not Park_ride or st['param'][x][3] == 0]:
+		near = set(ix.intersects(buffRect(G.vertex(i).point(), b_res))) & valid
+		if len(near) > 0 and st['param'][i][4] > min([st['cost'][x] for x in near]):
+			removenode.add(i)
+	valid = valid - removenode
 	
-	for i in list_acc:
-	
-		if not Park_ride or st['res'][i][n_Costvp] == 0:
-			near = index.intersects(buffRect(G.vertex(i).point(), b_res))
-			near = [x for x in near if st['l'][x] != -1]
-					
-			if len(near) > 0 and st['l'][i] > min([st['l'][x] for x in near]):
-				st['l'][i] = -1
-				list_acc.remove(i)
-
-
-    # built list of unique modes
-
-	for i in list_acc:
-		txt = st['res'][i][n_mode].split(sep)
-		if len(txt) == 0: st['res'][i][n_mode] = 'none'
-		elif len(txt) == 1: st['res'][i][n_mode] = txt
-		else: st['res'][i][n_mode] = sep.join(list(set(txt)))
-	
-	
+    # add park and ride cost
 	if Park_ride:
-	
-		for i in [x for x in list_acc if st['res'][x][n_Costvp] > 0]:
-					
-			res = st['res'][i][n_Costvp]
+
+		for i in [x for x in valid if st['param'][x][3] > 0]:					
+			near = set(ix.intersects(buffRect(G.vertex(i).point(), b_pr))) & valid
 			
 			# check if car time less than m_VP and less than max percentage of total time
-			
-			if res > m_VP or res > st['res'][i][n_Cost] * r_VP: st['l'][i] = -1
+			if st['param'][i][3] > m_VP or st['param'][i][3] > st['param'][i][4] * r_VP:
+				valid.discard(i)
 			
 			# else check if no faster bus node in buffer
+			elif len([j for j in near if st['cost'][j] < st['cost'][i]]) != 0:
+				valid.discard(i)
 			
+       		# else change node value to length to true length		
 			else:
-				near = index.intersects(buffRect(G.vertex(i).point(), b_pr))			
-				if len([j for j in near if st['l'][j] > 0 and st['l'][j]<st['l'][i]])!=0:
-					st['l'][i] = -1
-			
-       			# else change node value to length to true length
-					
-				else: st['l'][i] = st['res'][i][n_Cost]
+				st['cost'][i] = st['param'][i][4]
 
+    # built list of unique modes
+	for i in valid:
+		txt = st['param'][i][2].split(sep)
+		if len(txt) == 0: st['param'][i][2] = ''
+		elif len(txt) == 1: st['param'][i][2] = txt[0]
+		else: st['param'][i][2] = sep.join(list(set(txt)))
+			
+	# replace cost by -1 for non valid nodes
+	for i in nodes_d - valid:
+		st['cost'][i] = -1
+	
+	accnodes = accnodes | valid
 
 
 # Prepare results
 
-
 l = 0
+max_n = len(accnodes)
 node_feat = QgsFeature()
 
 fields = [
@@ -342,42 +308,41 @@ fields = [
 		  QgsField("modes", QVariant.String),
 		  QgsField("isClosest", QVariant.String)
 		  ]
-
 if Park_ride: fields.append(QgsField("driveCost", QVariant.Double))
-
 
 writer = VectorWriter(Results, None, fields, QGis.WKBPoint, networkprovider.crs()) 
 
 
-for k,v in Nodes.iteritems():
+for k,v in [(k,v) for k,v in Nodes.iteritems() if v in accnodes]:
 	progress.setPercentage(int(100 * l/max_n))
 	l+=1
-			
-	geom = QgsGeometry().fromPoint(G.vertex(Nodes[k]).point())
+				
+	geom = QgsGeometry().fromPoint(G.vertex(v).point())
 	node_feat.setGeometry(geom)
 	
-	minlst = [(s['l'][v], s['name']) for s in startpts if s['l'][v] != -1]
+	minlst = [(s['cost'][v], s['name']) for s in startpts if s['cost'][v] != -1]
 	
 	if len(minlst) != 0:
 		minst = min(minlst, key=itemgetter(0))[1]
 		
 	for start in startpts:
-		if start['l'][v] != -1:
+		if start['cost'][v] != -1:
 			attrs = [
 					  k,
 					  start['name'],
-					  start['l'][v],
-					  int(start['res'][v][n_corr]),
-					  start['res'][v][n_t_corr],
-					  start['res'][v][n_mode]
+					  start['cost'][v],
+					  int(start['param'][v][0]),
+					  start['param'][v][1],
+					  start['param'][v][2],
+					  'N'
 					]
 			
-			if start['name'] == minst: attrs.append('Y')
-			else: attrs.append('N')
-			
-			if Park_ride: attrs.append(start['res'][v][n_Costvp])
+			if start['name'] == minst: attrs[6] = 'Y'
+			if Park_ride: attrs.append(start['param'][v][3])
 				
 			node_feat.setAttributes(attrs)
 			writer.addFeature(node_feat)
 
 del writer
+
+progress.setText("{0:.1f} secs".format(time.time() - t))
